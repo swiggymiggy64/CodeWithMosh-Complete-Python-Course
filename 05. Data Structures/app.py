011c165c9f7640e36207b70d9c61247383fc1966
Michael Williams <104996526+swiggymiggy64@users.noreply.github.com>
2025-10-03 19:36:23 +0100

    Update app.py

--- 05. Data Structures/app.py
+++ 05. Data Structures/app.py
@@ -70,3 +70,96 @@
 #     # print(letter)
 #     # print(letter[0], letter[1])
 #     print(index, letter)
+
+# * Adding or Removing Items
+
+# letters = ["a", "b", "c"]
+
+# # Add
+
+# letters.append("d")  # Adds to the end of the list
+# letters.insert(0, "1")  # Specify the index & then item to add
+
+# # Remove
+# letters.pop()  # Remove the last item in the list
+# letters.pop(0)  # Or remove at specific index
+# letters.remove("b")  # Removes the 1st occurance of argument found in list
+# del letters[0:1]  # Remove a range of items
+# letters.clear()  # Remove ALL items from list
+
+# print(letters)
+
+# * Finding Items
+
+# letters = ["a", "b", "c"]
+
+# a = letters.index("a")
+# print(a)
+
+# # d = letters.index("d")
+# # print(d)  # This causes an error: ValueError: 'd' is not in list.
+
+# # So we can instead check if "d" exists first like this:
+# if "d" in letters:
+#     d = letters.index("d")
+#     print(d)
+
+# d_count = letters.count("d")
+
+# * Sorting Lists
+
+# numbers = [3, 51, 2, 8, 6]
+# # numbers.sort()
+# # numbers.sort(reverse=True)
+# sorted_numbers = sorted(numbers)
+# print(numbers)
+# print(sorted_numbers)
+
+# items = [
+#     ("Product1", 10),
+#     ("Product2", 9),
+#     ("Product3", 12),
+# ]
+
+
+# def sort_item(item):
+#     return item[1]
+
+
+# items.sort(key=sort_item)
+# print(items)
+
+# * Lambda Functions
+# In the above code we can use Lambda Function or Lambda Expression to make the code cleaner
+
+# items = [
+#     ("Product1", 10),
+#     ("Product2", 9),
+#     ("Product3", 12),
+# ]
+
+# # Instead of using this function
+# # def sort_item(item):
+# #     return item[1]
+
+# # We can tell sort() to use an lambda/anonymous function & then do the following "parameters:expression"
+# items.sort(key=lambda item: item[1])
+# print(items)
+
+# * Map Functions
+
+items = [
+    ("Product1", 10),
+    ("Product2", 9),
+    ("Product3", 12),
+]
+
+# Instead of doing this:
+# prices = []
+# for item in items:
+#     prices.append(item[1])
+# print(prices)
+
+# You can do this
+prices = list(map(lambda item: item[1], items))
+print(prices)
