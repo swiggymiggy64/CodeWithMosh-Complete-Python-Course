7b8a34ad7077713df41d28bb1c5d8c4ff55e68e3
Michael Williams <104996526+swiggymiggy64@users.noreply.github.com>
2025-10-17 18:47:12 +0100

    update

--- 11. Popular Python Packages/app.py
+++ 11. Popular Python Packages/app.py
@@ -104,3 +104,100 @@
 # browser.quit()
 
 # * Working with PDFs
+
+# import PyPDF2
+
+# # with open("first.pdf", "rb") as file:
+# #     reader = PyPDF2.PdfFileReader()
+# #     print(reader.numPages)
+# #     page = reader.getPage(0)
+# #     page.rotateClockwise(90)
+# #     writer = PyPDF2.PdfFileWriter()
+# #     writer.addPage(page)
+# #     # writer.insertPage(page, 0)
+# #     with open("rotated.pdf", "wb") as output:
+# #         writer.write(output)
+
+# merger = PyPDF2.PdfFileMerger()
+# file_names = ["first.pdf", "second.pdf"]
+# for file_name in file_names:
+#     merger.append(file_name)
+# merger.write("combined.pdf")
+
+# * Working with Excel Spreadsheets
+
+# pipenv install openpyxl
+# Change virtual env
+
+# import openpyxl
+
+# # wb = openpyxl.Workbook()
+# wb = openpyxl.load_workbook("transactions.xlsx")
+# print(wb.sheetnames)
+
+# sheet = wb["Sheet1"]
+
+# # wb.create_sheet("Sheet2", 0)
+# # wb.remove_sheet(sheet)
+
+# cell = sheet["a1"]
+# # for row in range(1, sheet.max_row + 1):
+# #     for column in range(1, sheet.max_column + 1):
+# #         cell = sheet.cell(row, column)
+# #         print(cell.value)
+
+# column = sheet["a"]
+# cells = sheet["a:c"]
+
+# print(column)
+# print(cells)
+
+# sheet.append([1, 2, 3])
+# # sheet.delete_cols()
+# wb.save("transactions2.xlsx")
+
+# * Command Query Separation Principle
+
+# import openpyxl
+
+# wb = openpyxl.load_workbook("transactions.xlsx")
+# sheet = wb["Sheet1"]
+# for row in range(1, 10):
+#     cell = sheet.cell(row, 1)
+#     print(cell.value)
+# sheet.append([1, 2, 3])
+# wb.save("transactions2.xlsx")
+
+# * NumPy
+
+# pipenv install numpy
+
+# import numpy as np
+
+# # # array = np.array([1, 2, 3])
+# # array = np.array([[1, 2, 3], [4, 5, 6]])
+# # print(array)
+# # print(type(array))
+# # print(array.shape)
+
+# # array = np.zeros((3, 4), dtype=int)
+# # array = np.ones((3, 4), dtype=int)
+# # array = np.full((3, 4), 5, dtype=int)
+# # array = np.random.random((3, 4))
+# # print(array)
+# # # print(array[0, 0])
+# # # print(array > 0.2)
+# # # print(array[array > 0.2])
+# # # print(np.sum(array))
+# # # print(np.floor(array))
+# # # print(np.ceil(array))
+# # print(np.round(array))
+
+# # first = np.array([1, 2, 3])
+# # second = np.array([1, 2, 3])
+# # print(first + second)
+# # print(first + 2)
+
+# dimensions_inch = np.array([1, 2, 3])
+# dimensions_cm = dimensions_inch * 2.54
+# print(dimensions_cm)
---
+++ 12. Building Web Applications with Django/app.py
@@ -0,0 +1,8 @@
+
+# * Intro
+
+# * Your First Django Project
+
+# pipenv install django==2.1
+# cd to vidly folder
+# django-admin startproject vidly .
